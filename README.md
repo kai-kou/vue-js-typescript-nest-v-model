af482f8bbcf2b82e0296a7240c4c0a966ba59bf0
kai <kai@cloudpack.jp>
2018-08-28 18:03:59 +0900

    Vue.js+TypeScriptでv-modelをネストさせるサンプルを実装した。

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # vue-js-typescript-nest-v-model
+
+Vue.js+TypeScriptでv-modelをネストするサンプル。
+
+## Usage
+
+```sh
+> git clone https://github.com/kai-kou/vue-js-typescript-nest-v-model.git
+> cd vue-js-typescript-nest-v-model/app
+> yarn
+> yarn serve
+```
+
+use Docker
+
+```sh
+> git clone https://github.com/kai-kou/vue-js-typescript-nest-v-model.git
+> cd vue-js-typescript-nest-v-model
+> docker-compose up -d
+> docker-compose exec app bash
+
+# cd app
+# yarn
+# yarn serve
+```
---
+++ app/src/components/ChildComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <input type="text" v-model="localValue"/>
+</template>
+
+<script lang="ts">
+  import {
+    Component,
+    Emit,
+    Prop,
+    Vue,
+  } from 'vue-property-decorator';
+  
+  @Component
+  export default class ChildComponent extends Vue {
+    @Prop() public value!: string;
+
+    @Emit()
+    public input(value: string) { /* */ }
+
+    private get localValue(): string {
+      return this.value;
+    }
+
+    private set localValue(value: string) {
+      this.input(value);
+    }
+  }
+</script>
